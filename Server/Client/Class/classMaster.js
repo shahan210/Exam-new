6b7d0c2ca810cd1a1844c7482387a018f9570f19
shahan210 <mohammedshahan210@gmail.com>
2024-05-04 14:00:38 +0530

    class master

--- Server/Client/Class/classMaster.js
+++ Server/Client/Class/classMaster.js
@@ -1,5 +1,6 @@
 import pool from "../../Config/config.js";
 
+// get all
 export const getAllClass = async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM classmaster");
@@ -49,6 +50,204 @@ export const getAllClass = async (req, res) => {
       });
     }
     // console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//create
+export const CreateClass = async (req, res) => {
+  const newData = req.body;
+  console.log(newData);
+  const Adddate = new Date().toISOString().slice(0, 19).replace("T", " ");
+  const autoID = Date.now() + Math.random();
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO classmaster (ClAutoId, QstClass , CLNAME, SECNAME,  AddedDate, AddedBy, IsActive) VALUES (?,?,?,?,?,?,?);",
+      [
+        autoID,
+        newData.QstClass,
+        newData.CLNAME,
+        newData.SECNAME,
+        Adddate,
+        newData.AddedBy,
+        newData.IsActive,
+      ]
+    );
+    if (result.length > 0) {
+      res.json({
+        data: [
+          {
+            ActionType: "",
+            ErrorMessage: "",
+            ErrorCode: "",
+            JSONData1: [result],
+            JSONData2: [],
+            JSONData3: [],
+            JSONData4: [],
+            JSONData5: [],
+            JSONData1Remarks: "",
+            JSONData2Remarks: "",
+            JSONData3Remarks: "",
+            JSONData4Remarks: "",
+            JSONData5Remarks: "",
+          },
+        ],
+        message: "successfull",
+        status: 200,
+      });
+    } else {
+      res.status(200).json({
+        data: [
+          {
+            ActionType: "",
+            ErrorMessage: "",
+            ErrorCode: "",
+            JSONData1: [result],
+            JSONData2: [],
+            JSONData3: [],
+            JSONData4: [],
+            JSONData5: [],
+            JSONData1Remarks: "",
+            JSONData2Remarks: "",
+            JSONData3Remarks: "",
+            JSONData4Remarks: "",
+            JSONData5Remarks: "",
+          },
+        ],
+        message: "no data found",
+      });
+      console.log(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get specific
+export const editClass = async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  try {
+    const result = await pool.query(
+      "SELECT * FROM classmaster WHERE classId = ?;",
+      [id]
+    );
+    if (result[0].length > 0) {
+      res.json({
+        data: [
+          {
+            ActionType: "",
+            ErrorMessage: "",
+            ErrorCode: "",
+            JSONData1: [result[0]],
+            JSONData2: [],
+            JSONData3: [],
+            JSONData4: [],
+            JSONData5: [],
+            JSONData1Remarks: "",
+            JSONData2Remarks: "",
+            JSONData3Remarks: "",
+            JSONData4Remarks: "",
+            JSONData5Remarks: "",
+          },
+        ],
+        message: "successfull",
+        status: 200,
+      });
+    } else {
+      res.status(200).json({
+        data: [
+          {
+            ActionType: "",
+            ErrorMessage: "",
+            ErrorCode: "",
+            JSONData1: [result[0]],
+            JSONData2: [],
+            JSONData3: [],
+            JSONData4: [],
+            JSONData5: [],
+            JSONData1Remarks: "",
+            JSONData2Remarks: "",
+            JSONData3Remarks: "",
+            JSONData4Remarks: "",
+            JSONData5Remarks: "",
+          },
+        ],
+        message: "no data found",
+      });
+      console.log(result[0]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//update
+export const updateClass = async (req, res) => {
+  const id = req.params.id;
+  const newData = req.body;
+  const ModifiedDate = new Date().toISOString().slice(0, 19).replace("T", " ");
+  try {
+    const result = await pool.query(
+      "UPDATE classmaster SET QstClass = ?, CLNAME = ?, SECNAME = ?, ModifiedDate = ?, ModifiedBy = ?, IsActive = ? WHERE ClassId = ?;",
+      [
+        newData.QstClass,
+        newData.CLNAME,
+        newData.SECNAME,
+        ModifiedDate,
+        newData.ModifiedBy,
+        newData.IsActive,
+        id,
+      ]
+    );
+    console.log(result);
+    if (result.length > 0) {
+      res.json({
+        data: [
+          {
+            ActionType: "",
+            ErrorMessage: "",
+            ErrorCode: "",
+            JSONData1: [result[0]],
+            JSONData2: [],
+            JSONData3: [],
+            JSONData4: [],
+            JSONData5: [],
+            JSONData1Remarks: "",
+            JSONData2Remarks: "",
+            JSONData3Remarks: "",
+            JSONData4Remarks: "",
+            JSONData5Remarks: "",
+          },
+        ],
+        message: "successfull",
+        status: 200,
+      });
+    } else {
+      res.status(200).json({
+        data: [
+          {
+            ActionType: "",
+            ErrorMessage: "",
+            ErrorCode: "",
+            JSONData1: [result[0]],
+            JSONData2: [],
+            JSONData3: [],
+            JSONData4: [],
+            JSONData5: [],
+            JSONData1Remarks: "",
+            JSONData2Remarks: "",
+            JSONData3Remarks: "",
+            JSONData4Remarks: "",
+            JSONData5Remarks: "",
+          },
+        ],
+        message: "no data found",
+      });
+      console.log(result[0]);
+    }
   } catch (error) {
     console.log(error);
   }
--- Server/Router/Class/Router.js
+++ Server/Router/Class/Router.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { getAllClass } from "../../Client/Class/classMaster.js";
+import {
+  CreateClass,
+  editClass,
+  getAllClass,
+  updateClass,
+} from "../../Client/Class/classMaster.js";
 
 const router = express.Router();
 
 router.get("/class", getAllClass);
+router.post("/class", CreateClass);
+router.get("/class/:id", editClass);
+router.put("/class/:id", updateClass);
 
-export default router
+export default router;
