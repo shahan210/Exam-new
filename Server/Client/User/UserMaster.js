12882832c1d93665e8634a243717f049b678cfce
shahan210 <mohammedshahan210@gmail.com>
2024-05-06 17:54:50 +0530

    login

--- Server/Client/User/UserMaster.js
+++ Server/Client/User/UserMaster.js
@@ -53,3 +53,56 @@ export const getUsers = async (req, res) => {
     console.log(error);
   }
 };
+export const getRights = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM adminuserrights");
+    if (result[0].length > 0) {
+      res.json({
+        data: [
+          {
+            ActionType: "",
+            ErrorMessage: "",
+            ErrorCode: "",
+            JSONData1: result[0],
+            JSONData2: [],
+            JSONData3: [],
+            JSONData4: [],
+            JSONData5: [],
+            JSONData1Remarks: "",
+            JSONData2Remarks: "",
+            JSONData3Remarks: "",
+            JSONData4Remarks: "",
+            JSONData5Remarks: "",
+          },
+        ],
+        message: "successfull",
+        status: 200,
+      });
+      // console.log(result);
+    } else {
+      res.status(200).json({
+        data: [
+          {
+            ActionType: "",
+            ErrorMessage: "",
+            ErrorCode: "",
+            JSONData1: result[0],
+            JSONData2: [],
+            JSONData3: [],
+            JSONData4: [],
+            JSONData5: [],
+            JSONData1Remarks: "",
+            JSONData2Remarks: "",
+            JSONData3Remarks: "",
+            JSONData4Remarks: "",
+            JSONData5Remarks: "",
+          },
+        ],
+        message: "no data found",
+      });
+    }
+    // console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
