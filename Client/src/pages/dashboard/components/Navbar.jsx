f0224ed0f2c7b80ed7f697a09d28902d1227a825
unais-k <unaissafa008@gmail.com>
2024-05-07 09:45:59 +0530

    layout changed

--- Client/src/pages/dashboard/components/Navbar.jsx
+++ Client/src/pages/dashboard/components/Navbar.jsx
@@ -1,16 +1,15 @@
-import React from "react";
 import School from "../../../assets/jpg/school.jpg";
 import Master from "../../../assets/jpg/master.jpg";
 
 const Navbar = () => {
-  return (
-    <div className="bg-slate-100 text-black w-full sticky top-0 z-50 p-2">
-      <div className="flex justify-between gap-4">
-        <img src={School} alt="" className="w-[200px] h-12" />
-        <img src={Master} alt="" className="w-[200px] h-12" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="fixed z-[20] md:ml-72 left-0 right-0 top-0 p-4 bg-background/80 backdrop-blur-md flex  gap-4 items-center border-b-[1px] ">
+            <div className="flex justify-between gap-4">
+                <img src={School} alt="" className="w-[200px] h-12" />
+                <img src={Master} alt="" className="w-[200px] h-12" />
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
